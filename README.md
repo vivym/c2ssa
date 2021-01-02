1ed3c659599a77b61e7d7970ea04585d084faf86
Vivym <1013114323@qq.com>
2021-01-02 19:08:31 +0800

    add README

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # C2SSA
+## 1. Compile LLVM from scrach
+
+```bash
+git clone https://github.com/llvm/llvm-project.git
+cd llvm-project
+mkdir build
+cd build
+cmake -DLLVM_ENABLE_PROJECTS="clang;clang-tools-extra" -DCMAKE_BUILD_TYPE=Debug ../llvm
+make -j8
+```
+
+## 2. Compile C2SSA
+
+```bash
+git clone https://github.com/vivym/c2ssa.git
+cd c2ssa
+mkdir build
+cd build
+cmake -DLLVM_ROOT_DIR=/Users/viv/Proj/llvm-project ..
+make
+```
+
+## 3. Run C2SSA
+
+```bash
+./c2ssa xx.c
+```
--- src/main.cpp
+++ src/main.cpp
@@ -12,20 +12,6 @@
 #include "CodeGenAction.h"
 #include <vector>
 
-/*
-// Apply a custom category to all command-line options so that they are the
-// only ones displayed.
-static llvm::cl::OptionCategory C11SSACategory("c2ssa options");
-
-// CommonOptionsParser declares HelpMessage with a description of the common
-// command-line options related to the compilation database and input files.
-// It's nice to have this help message in all tools.
-static llvm::cl::extrahelp CommonHelp(clang::tooling::CommonOptionsParser::HelpMessage);
-
-// A help message for this specific tool can be added afterwards.
-static llvm::cl::extrahelp MoreHelp("\nMore help text...\n");
-*/
-
 int main(int argc, const char **argv) {
   // clang::tooling::CommonOptionsParser OptionsParser(argc, argv, C11SSACategory);
   std::vector<const char *> CommandLine;
